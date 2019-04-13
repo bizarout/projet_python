b8b177f3f50ad1f0a426d063292c08702551a8aa
bizarout <hamid.bezzout.tech@gmail.com>
2019-04-13 16:23:21 +0200

    modification rabat.py

--- rabat.py
+++ rabat.py
@@ -1,1 +1,11 @@
-print(" Bonjour rabat")
+quote = ["le vrai malheur c'est d'être en valeur ", " l'or c'est toujour gagnant"," le danger reste une condition"," la santé est degradable", "le machinism est une philosophie"]
+
+reponse_utilisater = "C"
+if reponse_utilisater == "B":
+	pass
+elif reponse_utilisater == "C":
+	print(" c'est la bonne reponse.C..")
+
+
+else: 
+	pass
