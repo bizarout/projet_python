55f634d9004819d1505cdc27ccc858152cdff604
bizarout <hamid.bezzout.tech@gmail.com>
2019-04-15 22:51:28 +0200

    ajout fichier json et modifications de fichier rabat.py

--- rabat.py
+++ rabat.py
@@ -1,21 +1,37 @@
+import random
+import json
+
 quote = ["le vrai malheur c'est d'être en valeur ", "l'or c'est toujour gagnant","le danger reste une condition",""
 																												 "la santé est degradable", "le machinism est une philosophie"]
 
 reponse_utilisater = input( " entrer un caractere : 	")
 
+def read_vaue_from_json():
+	values = []
+	with open('characters.json') as f:
+		data =json.load(f)
+		for entry in data :
+			values.append(entry['quote'])
+	return values
+
+def random_character():
+	all_values = read_vaue_from_json()
+	return show_random_quote(all_values)
+
+
 
 def show_random_quote(my_list):
-	quote = my_list[1]
+	rand_num = random.randint(0, len(my_list)-1)
+	quote = my_list[rand_num]
 	return  quote
 
 while (reponse_utilisater != "B"):
-	print(show_random_quote(quote))
+	print(show_random_quote(quote),random_character())
 	reponse_utilisater = input(" entrer un caractere : 	")
 
 
-for item in quote:
-	item_c = item.capitalize()
-	print(item_c)
+
+
 
 
 
